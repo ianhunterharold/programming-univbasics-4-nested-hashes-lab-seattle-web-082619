70ec6b2d674062218b3c4667eb3e01ec17c43ee0
Ian Harold <ianhunterharold@gmail.com>
2019-07-29 19:45:00 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -24,6 +24,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+
 	
 	programmer_hash = 
  		{
@@ -40,6 +41,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     
+     return 	programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
